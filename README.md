2ea3c1d5ab2dd192dac39f6db71fccb98ce6881d
Frank Gonzalez-M <frank.gonzalez-morphy@bmw-m.com>
2023-04-28 22:32:27 +0200

    Commiting Submission Content
    All worked out is not only at the Udacity Workspace, it will be also saved online at GitHub.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# udacity-course-proj-final-capstone 🌎:
-Closing Project "Capstone" at EMR Cluster &amp; Spark on Hadoop (AWS) 24.04.2023 for Udacity Data Engineer Nanodegree.
+# Data Engineering's Project CAPSTONE :earth_americas:
+Closing Project "Capstone" on a EMR Cluster &amp; Spark on Hadoop (AWS - Amazon Web Services) of the **UDACITY Nanodegree in DATA ENGINEERING** Course.
 
-## Summary ℹ️
+## Overview :information_source:
 * [Introduction](#introduction)
 * [Data Schema](#data-schema)
 * [Files list](#files-list)
@@ -9,55 +9,99 @@ Closing Project "Capstone" at EMR Cluster &amp; Spark on Hadoop (AWS) 24.04.2023
 * [Getting Started](#getting-started)
 
 
-## Introduction ✏️:
+## Introduction :pencil2:
 
-I am running my Capstone project with public data from "Inside Airbnb", in specific from Mexico City. 
-All the files required are downloadable from [https://insideairbnb.com](http://insideairbnb.com/get-the-data/). 
+I am running my Capstone project with public data from "Inside Airbnb", in specific with the data of Mexico City. 
+All the files required are downloadable from [https://insideairbnb.com/get-the-data/](https://insideairbnb.com/get-the-data/).  
+
+**Hint:** The compressed files `(*.gz)` could be uploaded directly into workspace. On the exploration Jupyter Notebook there are a set of comands to uncompress the files directly on the (Udacity) Jupyter Workspace. 
+
+<br>
+
+| FILE NAME | DESCRIPTION | SIZE |
+| ----- | ----- | ----- | 
+| calendar.csv.gz | Detailed Calendar Data | 26.5 MB > 463 MB | 
+| listings.csv | Summary information and metrics for listings in Mexico City (good for visualisations) | 3.75 MB |
+| listings.csv.gz | Detailed Listings Data | 15.4 MB > 60.7 | 
+| neighbourhoods.csv | Neighbourhood list for geo filter. Sourced from city or open source GIS files. | 275 B | 
+| reviews.csv | Summary Review data and Listing ID (to facilitate time based analytics and visualitaion linekd to a listing) | 18.5 MB | 
+| reviews.csv.gz | Detailed Review Data | 86.7 MB > 226 MB | 
+
+_Worked with last update of data: 29 March 2023_
+<br>
 
 ![mexico-city](images/IMAGE-Airbnb-Mexico-City-ZoomedIn.png)
 
-I select to setup my project with `Spark` (an EMR at Amazon Web Services), a `JUPITER Workbook` (steps and results are directly documented) and for a direct execution of the ETL an `etl.py` Python file will be available.
-At the execution of the JUPITER Workbook it will be `pyspark` used, instead of using Redshift and Airflow.
-Looking about the cost factor Redshift is expensive, for another we can load the cleaned data from s3 with Spark easily and do big data analytics. 
-The Airbnb dataset will be updated monthly, so it may not be neccessary to build a pipeline to it. 
+### Choice of Tools & Technology for the project  
+
+**Common Intro**  
+The use of Pandas allows an easy data pre-processing as well visualistion (especially in the exploring phase case). It is helpful to efficiently load and manipulate data. Further in the project, instead of pandas dataframes ("_df"), it is recommended to use Spark dataframes ("df_") to allow distributed processing using for example Amazon's Elastic Map Reduce (EMR). 
+
+**My Selection**  
+I did select to setup my project with `Spark` (AWS Services), a `JUPYTER Notebook` for the exploration of the data (like a "Playground" to try steps and document results directly). A second Notebook for the final project executable project documentation in a clean way. For a direct execution of the ETL an `etl.py` Python file is also there.
+At the execution of the JUPYTER Workbook it will be `pyspark` used, instead of using Redshift and Airflow.  
+
+Looking about the cost factor Redshift is expensive, for another we can load the cleaned data on the fly without building tables and filling them to an from S3 with Apache Spark is easy and does have the ability to handle large data volumes and perform big data analytics through his distributed processing. This is a great advantage of using Spark on this!  
+
+The Inside Airbnb dataset is more a static dataset, less frequently as hourly or daily. An update monthly is enough. 
 
 [top](#summary)
 
-## Data Schema ✨:
+## Data Schema :sparkles:
 
 ### Schema design and ETL pipeline
 
-The star schema does consist of 1 *Fact* table (booking) and 4 *Dimension* tables (calendar, listing, neighbourhoods and reviews). The 4 source files in CSV format of **calendar**, **listings**, **neighbourhooods** as well of **reviews** are on the (local:) Udacity Workspace or (online:) at the S3 on AWS uploaded.
+A star schema as database design decision does allow a simple and effective way to organize data for querying and reporting. Because of the CSV-files there is a organisation in a way that favoured this.  
 
-![data-model](images/IMAGE-Data-Model.png)
+The star schema does consist of 1 *Fact* table (booking) and 4 *Dimension* tables (calendar, listing, neighbourhoods and reviews). For the *Dimension* tables they are 4 source files in CSV format of **calendar**, **listings**, **neighbourhooods** as well of **reviews** are on the (local:) Udacity Workspace or (online:) at the S3 on AWS uploaded.
+
+![data-model](images/IMAGE-Database-Model.png)  
+_Database Model Diagram draw with and sponsored by www.quickdatabaseddiagrams.com_
 
 
 ## Datasets used
-The datasets used are retrieved from the s3 bucket and are in CSV format. There are four datasets namely `calendar`, `listings`, `neighbourhoods` and `reviews`. 
-<font color="red">Mas mas y otro poquito mas...</font>
+The datasets used are retrieved (a) from the Udacity Workspace - used through the Jupyter Notebook or/and (b) from the s3 Bucket - used with the ETL execution of the `etl` python script. At the end, there are four datasets namely `calendar`, `listings`, `neighbourhoods` and `reviews`. 
+<font color="red">[***] Mas mas y otro poquito mas...</font>
 
 
-## Motivation ✏️:
-The intention of this project is at the end, have combined tables as source for a 'Self Service Report'-Dashboard done with PowerBI. 
+## Motivation :pencil2:
+The intention of this project is to provide to an analytics team access to the data model that allows easily to query and extract insights. It means at the end, have combined tables as a efficient and fast source for a 'Self Service Report'-Dashboard to be done with Microsoft Power BI (my daily work) or maybe featured on [AWS QuickSight](https://aws.amazon.com/de/quicksight/).  
+
+**Tipp:** Microsoft is bringing in Mai '23 a library which does allow to compell [Microsoft Power BI reports DIRECTLY at the Jupyter Notebook](https://powerbi.microsoft.com/en-us/blog/create-power-bi-reports-in-jupyter-notebooks/s) (_Whou!, absolutely a game changer_).
 
 
-## Files list 📎:
+## Files list :paperclip:
 
-Use this files for creation of the database Sparkifydb withhin the JUPITER Workplace with the required tables.  
+Use this files for creation of the database Sparkifydb withhin the JUPYTER Workplace with the required tables.  
 
 Following is a list or required files to execute the project: 
-* **aws-access.cfg** - configuration properties for (AWS) remote execution
-* **etl.py** - main python script with `function_A()`, `process_data_calendar()` and `process_data_listings()`, <font color="red">y algo mas...</font>
 * **README.md** - this file as project description
-* **Capstone-Notebook.ipynb** - Jupyter Workbook: Data Exploration as well an easy execution of ALL steps and scripts
+* **aws-access.cfg** - configuration properties for (AWS) remote execution
+* **Capstone-A-Exploration-Notebook.ipynb** - Jupyter Workbook: Data Exploration as well an easy execution of ALL (separated) steps and scripts
+* **Capstone-B-Project-Notebook.ipynb** - Jupyter Workbook: Clean Project description with some representative exections  
+* **Data-Dictionary-Inside-Airbnb-(Mexico-City).pdf** - An overview of Data Dictionary and Description of columns  
+* **etl.py** - Main python script with the processes `create_spark_session`, `read_csvs`, `data_preparation`, `data_types`, `pre_processing_s3`, `write_to_s3` and `data_quality_check`
 
-## Conclusion 🏁:
 
+
+## Conclusion :checkered_flag:
+
+To say in one word? "Heavy"! This was really a good challenge because of the tricky war to examine and explore data, like a blind. You will only see parts of the content and as you move forward, your will see more ... or less :-/  
+It was a good challange to explore your own way to move forward on the project! All depends also from the availibility of the IT-Systems. If you work directly on the Jupyter Workbook on the Udacity Environment, it depends on how the Notebook is opened. Does it open in a separated browser tab, den spontanously the Notebook will lost connection, kernel does not work anymore, you can not save and sometimes get scarry, but stay cool, on the other tab just refresh and restart the Udacity Workspace and then you could again save. My recomendation to you: Use a Version label and download time-to-time a writen version of your Notebook, to be save you have still your developed stuff.  
+
+From tooling it wasn't so complicated. The only is, that depending of the size of the data ( in my case ~4 Mio rows ) there will be lot of time outs and you need to restart the kernel and execute your Jupyter Notebook. By the way, fantastic possibility to execute code and get the results inmediately documented. 
+I work a lot with "small" project, and here was also a challenge to keep the local repository updated with my personal backup and also commiting stuff to the online GutHub repository. As I do not use "git" everyday get the chance to use it and keep "fresh" on the use, then it will be step-by-step simpler.  
+
+It was very interesting to work with the "Airbnb" data, and if you find data from your city then you will get more familiarized with the purpose and way on how to handle it. Finally, the whole importing, data verification, as well dynamically data modelling (including relationships between tables) was very very easier for my to do it in my daily business tool, Microsoft Power BI. It does help a lot to understand the way, content and problems handling data.  
+
+Hope you guys and girls outside in the programming world, to give with this also a guidance and an overview of the (possible) content of the project!  
+
+Thanks Udacity!
 
 
 [top](#summary)
 
-## Getting Started 📖:
+## Getting Started :book:
 
 ### Prerequisites
 
@@ -81,7 +125,7 @@ To create an Elastic Map Reduce (EMR) data lake on AWS, use the following steps:
 
 3. We will be using the default security group and the default EMR role `EMR_DefaultRole` for our purposes. Ensure that default security group associated with Master node has SSH inbound rule open, otherwise you won't be able to ssh. 
 
-![emr-setup](images/IMAGE-EMR-Setup.jpg)
+![emr-setup](images/IMAGE-EMR-Setup.png)
 
 
 ### Terminal commands 
@@ -98,7 +142,7 @@ To create an Elastic Map Reduce (EMR) data lake on AWS, use the following steps:
 [top](#summary)
 
 
-## Author 🕵️‍♂️:
+## Author :male_detective:
 
 Best regards  
 UDACITY [Data-Engineering]  
--- etl.py
+++ etl.py
@@ -1,5 +1,420 @@
-# """ ETL Main File """ 
- 
+# """ ETL Main File """
+import configparser              # required for reading out config-file [*.cfg]
+from datetime import datetime    # 
+import os                        # required for setting environment variables, like IAM-Authentification
+
+import numpy as np
+import matplotlib.pyplot as plt  # Pyplot - Bar Graph, Line Graph, Others (pending)
+
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import udf, col   #, monotonically_increasing_id
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+from pyspark.sql.types import StructType  as R,   StructField as Fld, \
+                              DoubleType  as Dbl, StringType  as Str, \
+                              IntegerType as Int, DateType    as Date, \
+                              BooleanType as Bol, DecimalType as Dec, \
+                              FloatType   as Flt, TimestampType as TS, \
+                              LongType    as Lng
+                              
+import pandas as pd
+
+
+
+def create_spark_session():
+    """HELP for procedure CREATE_SPARK_SESSION
+
+    Creation of or retrieves Spark Session
+    
+    Parameters: 
+    ( none )
+    """
+		
+		os.environ["JAVA_HOME"]   = "/usr/lib/jvm/java-8-openjdk-amd64"
+		os.environ["PATH"]        = "/opt/conda/bin:/opt/spark-2.4.3-bin-hadoop2.7/bin:" \
+		                            "/opt/conda/bin:/usr/local/sbin:/usr/local/bin:" \
+		                            "/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-8-openjdk-amd64/bin"
+
+		os.environ["SPARK_HOME"]  = "/opt/spark-2.4.3-bin-hadoop2.7"
+		os.environ["HADOOP_HOME"] = "/opt/spark-2.4.3-bin-hadoop2.7"
+
+		config = configparser.ConfigParser()
+		config.read('aws-access.cfg')
+		print(">> Read Out Config-Infos from [aws-access.cfg]")
+
+		os.environ['AWS_ACCESS_KEY_ID']     = config['AWS']['AWS_ACCESS_KEY_ID']
+		os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
+		
+		AWS_ACCESS_KEY_ID     = config['AWS']['AWS_ACCESS_KEY_ID']
+		AWS_SECRET_ACCESS_KEY = config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+		spark = SparkSession.builder \
+		                    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+		                    .config("spark.executor.instances", 10) \
+		                    .config("spark.executor.memory", "8g") \
+		                    .enableHiveSupport() \
+		                    .getOrCreate()
+
+		sc    = spark.sparkContext
+		sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", AWS_ACCESS_KEY_ID)
+		sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", AWS_SECRET_ACCESS_KEY)
+
+		# @ EMR (AWS): "emr.5.20.0" > "hadoop 2.7.0"
+		# @ EMR (AWS): "emr-5.31.0" > "hadoop 2.10.0"
+
+    return spark
+
+
+
+def read_csvs(input_data)
+    """HELP for procedure READ_CSVS
+    
+    Reads the CSV files needed for execution of project
+    Does return them as pandas dataframes for later data preparation
+
+    Parameters: 
+    - input_data: Data Directory source path
+    """
+    
+		# define (csv) filenames 
+		nhoods_csv_fn   = "neighbourhoods.csv"
+		list_csv_fn     = "listings.csv"
+		list_det_csv_fn = "listings_detailed.csv"  # < listings.csv.gz
+		calendar_csv_fn = "calendar.csv"           # < calendar.csv.gz
+		#reviews_csv_fn  = "reviews.csv"
+		rev_det_csv_fn  = "reviews_detailed.csv"   # < reviews.csv.gz
+		
+		hoods_csv   = input_data + nhoods_csv_fn
+		list_csv     = input_data + list_csv_fn
+		list_det_csv = input_data + list_det_csv_fn
+		calendar_csv = input_data + calendar_csv_fn
+		#reviews_csv  = input_data + reviews_csv_fn
+		rev_det_csv  = input_data + rev_det_csv_fn
+
+		print(">> Source path [" + nhoods_csv_fn   + "]: [" + hoods_csv   + "]")
+		print(">> Source path [" + list_csv_fn     + "]: [" + list_csv     + "]")
+		print(">> Source path [" + list_det_csv_fn + "]: [" + list_det_csv + "]")
+		print(">> Source path [" + calendar_csv_fn + "]: [" + calendar_csv + "]")
+		#print(">> Source path [" + reviews_csv_fn  + "]: [" + reviews_csv  + "]")
+		print(">> Source path [" + rev_det_csv_fn  + "]: [" + rev_det_csv  + "]")		
+    
+    #read csv file contents into pandas dataframe
+    hoods_df    = pd.read_csv(hoods_csv)
+    list_df     = pd.read_csv(list_csv)
+    list_det_df = pd.read_csv(list_det_csv)
+    calendar_df = pd.read_csv(calendar_csv)
+    reviews_df  = pd.read_csv(rev_det_csv)
+    		
+		return hoods_df, list_df, list_det_df, calendar_df, reviews_df, \
+		       nhoods_csv, list_csv, list_det_csv, calendar_csv, rev_det_csv
+
+
+
+def data_preparation(hoods_df, list_df, list_det_df, calendar_df, reviews_df)
+    """HELP for procedure DATA_PREPARATION
+    
+    Makes data cleaning and preparation of identified steps
+    See also single steps on jupyter notebook
+
+    Parameters: 
+    - Data Frames of different sources
+    """
+    
+		calendar_df['adjusted_price'] = calendar_df['adjusted_price'].str.replace('$', '')
+
+		list_df = list_df.fillna('')
+    null_to_zero = ['reviews_per_month','accomodates','bathrooms','bedrooms','beds']
+    for i in null_to_zero
+        list_df[i] = list_df[i].replace('', 0)
+
+    # Print the number of duplicate rows in each dataframe
+    duplicated_list_id = list_df[list_df.duplicated(['id'])]['id'].count()
+    print(Number of duplicate list_ids in list_df, duplicated_list_id)
+
+    duplicated_review_id = reviews_df[reviews_df.duplicated(['id'])]['id'].count()
+    print(Number of duplicate review_ids in review_df, duplicated_review_id)
+		
+		   
+
+def data_types()
+    """HELP for procedure DATA_TYPES
+    
+    Set wished data type to the dataframes before writing to S3.
+    Mostly the dataSchemas are pre-defined for the datas.
+
+    Parameters: 
+    (none)
+    """
+    
+    # define schema as data type is required
+    hoodSchema = R([
+		    Fld("neighbourhood_group", Str()),
+		    Fld("neighbourhood",       Str()),
+		])
+    
+		list_detSchema = R([
+		    Fld("id",                     Int()),
+		    Fld("name",                   Str()),
+		    Fld("description",            Str()),
+		    Fld("host_id",                Int()),
+		    Fld("host_name",              Str()),
+		    Fld("host_since",             Date()),
+		    Fld("source",                 Str()),
+		    Fld("latitude",               Flt()),
+		    Fld("longitude",              Flt()),
+		    Fld("price",                  Flt()),
+		    Fld("review_scores_rating",   Int()),
+		    Fld("reviews_per_month",      Flt()),
+		    Fld("room_type",              Str()),
+		    Fld("accommodates",           Int()),
+		    Fld("bathrooms",              Int()),
+		    Fld("bedrooms",               Int()),
+		    Fld("beds",                   Int()),
+		    Fld("neighbourhood_cleansed", Str()),
+		])
+    
+    # Alredy here cutting (cleaning up) columns ['price','m*']!
+    calendarSchema = R([
+		    Fld("listing_id",     Int()),
+		    Fld("date",           Date()),
+		    Fld("available",      Str()),
+		    Fld("adjusted_price", Str()),
+		])
+    
+    rev_detSchema = R([
+		    Fld("listing_id",    Int()),
+		    Fld("id",            Int()),
+		    Fld("date",          Date()),
+		    Fld("reviewer_id",   Int()),
+		    Fld("reviewer_name", Str()),
+		    Fld("comments",      Str()),
+		])
+
+    print(">> DataFrame Schemas are declared")
+    
+    return hoodSchema, list_detSchema, calendarSchema, rev_detSchema
+    
+    
+
+def pre_processing_s3(nhoods_csv, list_csv, list_det_csv, calendar_csv, rev_det_csv)
+    """HELP for procedure PRE_PROCESSING_S3
+    
+    Creation of the Spark DataFrames as basis for upload to S3 AWS Bucket.
+    Indirectly the tables bases.
+
+    Parameters: 
+    - The Names of sources
+    """
+
+		# read out and create spark dataframes ("_df"), NOT Data Feed ("df_")
+		df_nhoods   = spark.read.csv(nhoods_csv, hoodSchema)
+		
+		df_list     = spark.read.option("header", "true").format("csv").schema(listSchema).load(list_csv)
+		
+		df_listing  = spark.read.option("header", "true").format("csv") \
+		                   .schema(list_detSchema) \
+		                   .load(list_det_csv)
+
+		df_calendar = spark.read.option('header', 'true') \
+		                   .option('sep', ',') \
+		                   .format('csv') \
+		                   .schema(calendarSchema) \
+		                   .load(calendar_csv)
+		                   
+		#df_reviews  = spark.read.csv(reviews_csv)
+		df_rev_det  = rev_det_df = spark.read.format("csv") \
+		                   .option("header",    True) \
+		                   .option('quotes',    '"') \
+		                   .option("delimiter", ',') \
+		                   .schema(rev_detSchema) \
+		                   .load(rev_det_csv)
+		                   
+		
+		    
+    # Select columns for the various tables preparing upload to S3 as parquet
+    # +++
+    
+    # NEIGHBOURHOODS - extract columns to create Hoods table
+    hoods_table = df_hoods.select('neighbourhood_group', 'neighbourhood')
+
+    # LISTING - extract columns to create LISTING table
+    df_listing       = df_listing.withColumn('month', month('host_since'))
+    # Only using columns of interest (demo). Including all will slow down execution for review
+    listing_columns  = ['id','name','description','host_id','host_name','host_since', \
+                       'month','source','latitude','longitude','price', \
+                       'review_scores_rating','reviews_per_month','room_type', \
+                       'accommodates','bathrooms','bedrooms','beds','neighbourhood_cleansed']
+    listing_table    = df_listing.selectExpr(listing_columns).dropDuplicates()
+    
+    # CALENDAR - extract columns to create CALENDAR table
+    df_calendar      = df_calendar.withColumn('month', month('date'))
+    calendar_columns = ['listing_id', 'date', 'available', 'adjusted_price', 'month']
+    calendar_table   = df_calendar.selectExpr(calendar_columns).dropDuplicates()
+
+    # REVIEWS TABLE - extract columns to create REVIEWS table
+    df_reviews = df_reviews.withColumnRenamed("id",   "review_id") \
+                           .withColumnRenamed("date", "review_date")
+    df_reviews       = df_reviews.na.drop()
+    df_reviews       = df_reviews.withColumn('month', month('review_date'))
+    reviews_columns  = ['listing_id', 'review_id', 'review_date', \
+                        'reviewer_id', 'reviewer_name', 'comments', 'month']
+    reviews_table    = df_reviews.selectExpr(reviews_columns).dropDuplicates()
+
+
+    ## Creating the FACT table BOOKINGS now
+    df_bookings_stg  = df_calendar.join(listing_table, \
+                                        on = [calendar_table.listing_id == \
+                                            listing_table.id], \
+                                        how = 'left')
+    df_bookings      = df_bookings_stg.join(reviews_table, \
+                                        on = [df_bookings_stg.reviewer_id == \
+                                              reviews_table.reviewer_id], \
+                                        how = 'left')
+
+    # preparing BOOKINGS before uploading to the S3 as parquet
+    df_bookings      = df_bookings.withColumn('month', month('review_date'))
+    # Only using columns of interest (demo). Including all will slow down execution for review
+    booking_table    = df_bookings.select('id','name','description','host_id','host_name', \
+                         'host_since','source','latitude','longitude','adjusted_price', \
+                         'review_id','review_date','reviewer_id','review_scores_rating', \
+                         'reviews_per_month','room_type','accommodates','bathrooms', \
+                         'bedrooms','beds','month_review')
+                                      
+    print('>> Tables data is prepared and ready to be uploaded to S3')
+		
+
+
+def write_to_s3(output_bucket)
+    """HELP for procedure WRITE_TO_S3
+    
+    Procedure does write Spark DataFrame to s3 as Parquet files
+
+    Parameters: 
+    - output_bucket: target directory where to save data on S3 at AWS
+    """
+    
+    # WRITING TABLES AS PARQUET TO S3
+    
+    # write NEIGHBOURHOODS table to parquet files partitioned by neighbourhood_group
+    hoods_table.write.mode('overwrite').partitionBy('neighbourhood_group').parquet(output_bucket + 'neighbourhoods')
+    print('>> NEIGHBOURHOODS Table is created on the S3 bucket!')
+
+    # write LISTING table to parquet files partitioned by month and room type
+    listing_table.write.partitionBy('month', 'room_type') \
+                 .parquet(output_bucket + 'listing', 'overwrite')
+    print('>> LISTING Table is created on the S3 bucket!')
+
+    # write CALENDAR table to parquet files partitioned by month
+    calendar_table.write.partitionBy('month') \
+                  .parquet(output_bucket + 'calendar', 'overwrite')
+    print('>> CALENDAR Table is created on the S3 bucket!')
+
+    # write REVIEW table to parquet files partitioned by month 
+    reviews_table.write.partitionBy('month') \
+                 .parquet(output_bucket + 'reviews', 'overwrite')
+    print('>> REVIEW Table is created on the S3 bucket!')
+
+    # write BOOKINGS (fact) table to parquet files partitioned month
+    booking_table.write.partitionBy('month') \
+                 .parquet(output_bucket + 'booking', 'overwrite')
+    print('>> BOOKING Table is created on the S3 bucket!')
+    
+    
+
+def data_quality_check(output_data)
+    """HELP for procedure DATA_QUALITY_CHECK
+    
+    Reread from S3 the parquet files uploaded before.
+    A check will be runs, to see if the the amount of up-/downloaded rows does match
+
+    Parameters: 
+    - output_data: online source directory of parquet files on S3 at AWS
+    """
+
+    # From S3 read tables in order to parse and do analysis ...
+    neighbourhoods = spark.read.parquet(output_data + 'neighbourhoods')
+    listing        = spark.read.parquet(output_data + 'listing')
+    calendar       = spark.read.parquet(output_data + 'calendar')
+    reviews        = spark.read.parquet(output_data + 'reviews')
+    booking        = spark.read.option('mergeSchema', True).parquet(output_data + 'booking')
+    
+    # Get the count of rows in the dataframe
+    uploaded_table    = ['booking_table','listing_table','reviews_table','hoods_table','calendar_table']
+    downloaded_tables = ['booking','listing','reviews','neighbourhoods','calendar']
+
+    expected_count = []
+    row_count      = []
+
+    for i in uploaded_table:
+    	  expected_count.append(i)
+
+    for i in downloaded_tables:
+    	  row_count.append(i)
+
+    # Compare the counts
+    for uploaded, downloaded in zip(uploaded_table, downloaded_tables):
+    	    if downloaded.count() != uploaded.count():
+    	    	        raise ValueError(">> Data is incomplete. Expected {} rows in {} but found {}" \
+    	    	                         .format(uploaded.count(), uploaded, downloaded.count()) \
+    	    	                        )
+
+
+
+def main():
+    """HELP for ETL (main)
+
+    Extract songs and events data from S3, transform it into dimensional tables
+    format and uploads back into S3 in PARQUET format
+    
+    Subprocesses
+    - create_spark_session
+    - read_csvs
+    - data_preparation
+    - data_types
+    - pre_processing_s3
+    - write_to_s3
+    - data_quality_check
+    """
+    
+    print('>> START (main)')
+
+    # (A) creating Spark Session 
+    spark = create_spark_session()
+    print('>> spark session created')
+
+    # AWS Settings @ EMR Notebook Environment
+    ##output_data = "s3a://capstone-project-out/"
+    # + + + + + 
+    # LOC Setting @ Udacity Workbook Environment
+    input_data  = "./"   # "data/"
+    output_data = "data_output/"
+    
+    # (B) Assign Names of CSV files
+    [hoods_df, list_df, list_det_df, calendar_df, reviews_df, \
+     nhoods_csv, list_csv, list_det_csv, calendar_csv, rev_det_csv] = read_csvs(input_data) 
+    
+    # (C) Prepare Data 
+    data_preparation(hoods_df, list_df, list_det_df, calendar_df, reviews_df)
+    
+    # (D) Construct Data Schemas
+    [hoodSchema, list_detSchema, calendarSchema, rev_detSchema] = data_types()
+    
+    # (E) Read and Pre-Process Data
+    pre_processing_s3(nhoods_csv, list_csv, list_det_csv, calendar_csv, rev_det_csv)
+    
+    # (F) Write Data to S3 Bucket at AWS
+    write_to_s3(output_bucket)
+    
+    # (G) Make some Checks
+    data_quality_check(output_data)
+    
+
+    print('>> END! [etl.py] executed!')
+
+
+if __name__ == "__main__":
+    main()
  
 
 # +++ EOF +++ [etl.py] +++
